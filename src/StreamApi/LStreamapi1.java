88a81b2df2eb0fbd1b33e56b89ef29accb3bc329
vinaykumarVK <vinaykumarvanga.dataqsoft@gmail.com>
2026-08-20 17:35:56 +0530

    second commit

--- src/StreamApi/LStreamapi1.java
+++ src/StreamApi/LStreamapi1.java
@@ -8,13 +8,23 @@ class Even{
         return nums.stream().filter(n->n%2==0).reduce(0, Integer::sum);
     }
     int multi(List<Integer> nums){
-        return nums.stream().filter(i -> i%2==0).map(i ->i*2).forEach;
+        return nums.stream().filter(i -> i%2==0).map(i ->i*2).reduce(0, Integer::sum);
+    }
+    int max(List<Integer> nums){
+        return nums.stream().filter(i -> i%2==0).reduce(0, Integer::max);
+    }
+    int min(List<Integer> nums){
+        return nums.stream().filter(i -> i%2==0).reduce(0, Integer::min);
     }
 
 }
 public class LStreamapi1 {
-    static void main(String[] args) {
-        List<Integer> nums=List.of(1,3,5,6,7,9,10,12);
-
+    public static void main(String[] args) {
+        List<Integer> nums=List.of(1,2,3,5,6,7,9,10,12);
+        Even e=new Even();
+        System.out.println("Sum of even numbers = "+e.sum(nums));
+        System.out.println("Product of even numbers = "+e.multi(nums));
+        System.out.println("Maximum of even number = "+e.max(nums));
+        System.out.println("Minimum of even number = "+e.min(nums));
     }
 }
